e0f4aab2747228d05e4919b482c7c9009d6cbc84
ayarii <hamzaayari2026@gmail.com>
2025-09-28 14:56:46 +0100

    Added dark theme to profile managment page

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight theme-transition">
             {{ __('Profile') }}
         </h2>
     </x-slot>
 
-    <div>
+    <div class="bg-gray-100 dark:bg-gray-900 theme-transition">
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
