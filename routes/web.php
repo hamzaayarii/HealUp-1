2448c4168c01b8eb7344bc2b0313684f2f8d0d35
ayarii <hamzaayari2026@gmail.com>
2025-09-28 11:16:11 +0100

    Fix welcome page route to point to pages.welcome

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 })->name('welcome');
 
 // Our first project route
